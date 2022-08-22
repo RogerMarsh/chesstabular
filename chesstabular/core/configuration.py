1e88db7779ec3014b3595e4d0bd2f9a154e1e76b
Roger Marsh <roger.marsh@solentware.co.uk>
2022-08-22 08:28:45 +0000

    Change Configuration class to customise Configuration class from solentware_misc project.

--- chesstabular/core/configuration.py
+++ chesstabular/core/configuration.py
@@ -2,28 +2,31 @@
 # Copyright 2022 Roger Marsh
 # Licence: See LICENCE (BSD licence)
 
-# This module is a canditate for a move to the solentware_misc package.
-"""Access and update names of last used configuration files.
+"""Access and update items in a configuration file.
 
-The initial values are taken from file named in self._RESULTS_CONF in the
+The initial values are taken from file named in self._CONFIGURATION in the
 user's home directory if the file exists.
 
 """
+from solentware_misc.core import configuration
 
-import os
+import ecfformat.core.constants
 
-from chessvalidate.core import configuration
-
-from ..core import constants
+from . import constants
 
 
 class Configuration(configuration.Configuration):
     """Identify configuration file and access and update item values."""
 
-    _RESULTS_CONF = ".chesstabular.conf"
-    _DEFAULT_RECENTS = (
+    _CONFIGURATION = ".chesstabular.conf"
+    _DEFAULT_ITEM_VAULES = (
         (constants.RECENT_EMAIL_SELECTION, "~"),
         (constants.RECENT_EMAIL_EXTRACTION, "~"),
         (constants.RECENT_DOCUMENT, "~"),
         (constants.RECENT_TABULAR_EVENTS, "~"),
+        (ecfformat.core.constants.RECENT_RESULTS_FORMAT_FILE, "~"),
+        (
+            ecfformat.core.constants.SHOW_VALUE_BOUNDARY,
+            ecfformat.core.constants.SHOW_VALUE_BOUNDARY_TRUE,
+        ),
     )
